5c46ea76a7fabd9877b2b18dfc92e5f1c3b9b496
Андрей Кондратьев <127983333+KayHan04@users.noreply.github.com>
2024-02-18 19:17:55 +0400

    Update task.py

--- task.py
+++ task.py
@@ -9,7 +9,7 @@ data['b'] = str(input('Input b: '))
 data['c'] = str(input('Input c: '))
 
 with open('conf.txt','w') as f:
-    for value in sorted(data.items(),key=lambda item:item[1],reverse=True):
-        f.write('%s = %s\n' % (value))
+    for key, value in sorted(data.items(),key=lambda item:item[1],reverse=True):
+        f.write('%s = %s\n' % (key, value))
 
 print(open('conf.txt','r').read())
